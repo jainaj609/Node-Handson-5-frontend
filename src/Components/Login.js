79e83035fb4c207f4d7e80414bb1a368ee453b16
jainaj609 <jainaj609@gmail.com>
2023-05-28 03:54:45 +0530

    updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,6 +20,9 @@ const Login = () => {
                 <img src='https://t4.ftcdn.net/jpg/04/17/26/55/360_F_417265589_3icF8VsU9yT6mTWTAeFkGry8JKQdztxt.jpg' alt='logo' />
                 <input type='text' placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)} />
                 <button onClick={btnClick}>Login</button>
+                <div className='space'>
+                    Welcome to the chat APP
+                </div>
             </div>
         </div>
     )
